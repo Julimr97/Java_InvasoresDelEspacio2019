9f9ee61af70ce0bbac7d08fc10e827abe5b47385
juli <juli@10.10.21.127>
2019-03-18 08:50:08 +0100

    Autor: Julián Mora creamos fonde en negro con la nave que se mueve y el disparo y el marciano

---
+++ src/codigo/Marciano.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+import java.awt.Image;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author juli
+ */
+public class Marciano {
+      public Image imagen1, imagen2 = null;
+    public int x = 0;
+    public int y = 0;
+    private int vX = 1;
+    
+    
+    public Marciano (){
+        try {
+            imagen1 = ImageIO.read(getClass().getResource("/imagenes/marcianito1.png"));
+            imagen2 = ImageIO.read(getClass().getResource("/imagenes/marcianito2.png"));
+
+        } catch (IOException ex) {
+            
+    }
+ }    
+    
+        public void mueve(){
+            x+=vX;
+        }
+
+}
